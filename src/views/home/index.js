463f55f5850a7fe744c9eccc347d22dec2436797
Felipe Silveira <eng.fe.silveira@gmail.com>
2020-10-20 17:17:35 +0200

    feat: Upload image from device implemented

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -3,9 +3,20 @@ import "./_styles.scss"
 export default {
   name: "Home",
   data: () => ({
-    model: ""
+    model: "",
+    images: []
   }),
   methods: {
+    uploadImage(event) {
+      var reader = new FileReader();
+
+      reader.onload = () => {
+        var output = document.getElementById('output_image');
+        output.src = reader.result;
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
+    },
     async classify() {
       const mobilenet = require('@tensorflow-models/mobilenet');
       console.log('Successfully loaded mobilenet');
@@ -15,21 +26,26 @@ export default {
       console.log('Successfully loaded model');
 
       // Make a prediction through the model on our image
-      const imgEl = document.getElementById('img');
+      const imgEl = document.getElementById('output_image');
       const result = await this.model.classify(imgEl);
       console.log(result);
     }
   },
+
   render(h) {
     return (
-      <div class="home">
+      <div class="classifier">
         <h1>Online Image Classifier</h1>
 
-        {/* <!-- Add an image that we will use to test --> */}
-        <img id="img" class="img" crossorigin src="https://i.imgur.com/JlUvsxa.jpg" />
-        
-        {/* <!-- Add a classify button to trigger the function --> */}
-        <button class="btn" onclick={() => this.classify()}>Classify</button>
+        <div class="classifier__input">
+          <p>Please, upload an image</p>
+          <input type="file" accept="image/*" onchange={(event) => this.uploadImage(event)}></input>
+        </div>
+
+        <div class="classifier__image">
+          <img id="output_image" />
+          <button onclick={() => this.classify()}>Classify</button>
+        </div>
       </div>
     );
   }
