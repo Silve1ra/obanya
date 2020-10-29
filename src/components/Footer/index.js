2a86c13b1297038d4bfad019cdfaf87e28dd69d5
Felipe Silveira <eng.fe.silveira@gmail.com>
2020-10-29 14:50:18 +0100

    feat: implementing footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,47 @@
+export default {
+  name: "Footer",
+  data: () => ({
+    links: [
+      {name: 'Home'},
+      {name: 'About us'},
+      {name: 'Work'},
+      {name: 'Info'},
+      {name: 'Obanya'}
+    ],
+  }),
+  render(h) {
+    return (
+      <v-app>
+        <v-footer
+          padless
+        >
+          <v-row
+            justify="center"
+            no-gutters
+          >
+            {this.links.map(link => {
+              return(
+                <v-btn
+                  key={link.name}
+                  color="dark"
+                  text
+                  rounded
+                  class="my-2"
+                >
+                  { link.name }
+                </v-btn>
+              );
+            })}
+            
+            <v-col
+              class="lighten-2 py-4 text-center"
+              cols="12"
+            >
+              Palinovic 
+            </v-col>
+          </v-row>
+        </v-footer>
+      </v-app>
+    );
+  }
+}
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -2,13 +2,15 @@ export default {
   name: "Home",
   components: {
     Carousel: () => import("@/components/Carousel"),
-    Toolbar: () => import("@/components/Toolbar")
+    Toolbar: () => import("@/components/Toolbar"),
+    Footer: () => import("@/components/Footer")
   },
   render(h) {
     return(
       <div>
         <Toolbar/>
         <Carousel/>
+        <Footer/>
       </div>
     );
   }
