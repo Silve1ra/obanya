89554b84fcdc7fcee7556da10ff595992312c50a
Felipe <eng.fe.silveira@gmail.com>
2020-11-16 21:36:49 +0100

    feat: adding pollen families to hero

--- src/components/home/Hero/index.js
+++ src/components/home/Hero/index.js
@@ -18,6 +18,25 @@ export default {
             sm="2">
             Obanya App is destinated to help biologists, botanics, palynologysts and so on, to automatically classify, detect and count pollen grains through images 
           </v-col>
+
+          <v-col
+            class="content-info"
+          >
+          version 1.0.1
+          <br/>
+          The following pollen families are supported:
+          <ol>
+            <li>Anadenanthera</li>
+            <li>Arecaceae</li>
+            <li>Cecropia</li>
+            <li>Myrcia</li>
+            <li>Poaceae</li>
+            <li>Protium</li>
+            <li>Schinus</li>
+            <li>Serjania</li>
+            <li>Syagrus</li>
+          </ol>
+          </v-col>
         </div>
       </div>
     );
